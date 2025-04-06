825b4b5ebfeaf5e85506f7ed6f92d206172cb16d
Jharvy <jharvy.cadillo.t@uni.pe>
2025-04-06 10:48:08 -0500

    Se agrego los comentarios de stash en main

--- Actividad_04/main.py
+++ Actividad_04/main.py
@@ -4,8 +4,5 @@ print('Hello World - update in main')
 def greet():
     print('Fixed bug in feature')
 print('Cherry pick this')
-<<<<<<< Updated upstream
 #Este cambio si sera confirmado
-=======
 #This change is stashed
->>>>>>> Stashed changes
