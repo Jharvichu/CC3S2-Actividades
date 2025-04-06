9ebb2a565c2a202d96ddbbbc2aeec692b60177f2
Jharvy <jharvy.cadillo.t@uni.pe>
2025-04-06 10:42:52 -0500

    Se agrego los cambios guardado con stash

--- Actividad_04/main.py
+++ Actividad_04/main.py
@@ -4,4 +4,8 @@ print('Hello World - update in main')
 def greet():
     print('Fixed bug in feature')
 print('Cherry pick this')
+<<<<<<< Updated upstream
 #Este cambio si sera confirmado
+=======
+#This change is stashed
+>>>>>>> Stashed changes
